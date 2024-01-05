fba4daa1a42c48baaf574d8f388c1b9e2b0a02cb
Mustafa Yigit Uzun <mustafa.uzun@kolaybi.com>
2024-01-05 15:42:22 +0300

    Add profiles relation to NumeratorType model
    (cherry picked from commit 0597223a5b737e9ea724cebb473c1ce63197a4fe)

--- src/Models/NumeratorType.php
+++ src/Models/NumeratorType.php
@@ -3,14 +3,18 @@
 namespace KolayBi\Numerator\Models;
 
 use Illuminate\Database\Eloquent\Casts\Attribute;
+use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Eloquent\Concerns\HasUlids;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 /**
  * @property string $id
  * @property string $name
  * @property int    $min
  * @property int    $max
+ *
+ * @property-read Collection<NumeratorProfile> $profiles
  */
 class NumeratorType extends Model
 {
@@ -30,5 +34,10 @@ class NumeratorType extends Model
         return Attribute::make(
             get: fn(mixed $value) => $value ?? PHP_INT_MAX,
         );
+    }
+
+    public function profiles(): HasMany
+    {
+        return $this->hasMany(NumeratorProfile::class, 'type_id', 'id');
     }
 }
