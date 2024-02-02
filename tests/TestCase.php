8f2504cb823e61abd244694fc11047d0ae3704ab
Mustafa Yigit Uzun <mustafa.uzun@kolaybi.com>
2024-02-02 08:52:54 +0300

    Create abstract TestCase class
    (cherry picked from commit f626966012c89a3afe97cda05f9b3f6146a0c12b)

---
+++ tests/TestCase.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace KolayBi\Numerator\Tests;
+
+use Exception;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use KolayBi\Numerator\NumeratorServiceProvider;
+use Orchestra\Testbench\TestCase as OrchestraTestCase;
+
+abstract class TestCase extends OrchestraTestCase
+{
+    use RefreshDatabase;
+
+    /**
+     * @throws Exception
+     */
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->withFactories(__DIR__ . '/../database/factories');
+    }
+
+    protected function getPackageProviders($app): array
+    {
+        return [
+            NumeratorServiceProvider::class,
+        ];
+    }
+
+    protected function getEnvironmentSetUp($app): void
+    {
+        $app['config']->set('database.default', 'testdb');
+        $app['config']->set('database.connections.testdb', [
+            'driver'   => 'sqlite',
+            'database' => ':memory:',
+        ]);
+    }
+}
