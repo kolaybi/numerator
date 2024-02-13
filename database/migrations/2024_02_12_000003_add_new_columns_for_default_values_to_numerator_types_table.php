bc4d292adda29ea23db5fd8df18e5daabc24964d
Mustafa Yigit Uzun <mustafa.uzun@kolaybi.com>
2024-02-13 11:14:01 +0300

    Create NumeratorType default values migration

---
+++ database/migrations/2024_02_12_000003_add_new_columns_for_default_values_to_numerator_types_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Config;
+use Illuminate\Support\Facades\Schema;
+
+return new class () extends Migration {
+    public function up(): void
+    {
+        $prefixColumnLength = Config::get('numerator.database.prefix_length', 3);
+        $suffixColumnLength = Config::get('numerator.database.suffix_length', 3);
+
+        Schema::table('numerator_types', function (Blueprint $table) use ($prefixColumnLength, $suffixColumnLength) {
+            $table->string('prefix', $prefixColumnLength)->nullable();
+            $table->string('suffix', $suffixColumnLength)->nullable();
+            $table->string('format')->nullable();
+            $table->unsignedTinyInteger('pad_length')->nullable();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::table('numerator_types', function (Blueprint $table) {
+            $table->dropColumn(['prefix', 'suffix', 'format', 'pad_length']);
+        });
+    }
+};
